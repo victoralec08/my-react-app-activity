048ba06157fd613a8ba02f18b5e995aac8bfb4ff
alec.victor <victoralec08@gmail.com>
2023-05-04 19:48:12 +0800

    Create initial layout for component 2

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -9,17 +9,23 @@ const AppStyles = createTheme({
         html,
         body {
             background-color: #0e1414;
-            color: #bfc1c1;
+            color: #fff;
             height: 100vh;
         }
 
         .bodyContainer {
             padding: 150px 0px;
         }
-        .component1Container{
+
+        .index-containter{
             max-width: 1210px;
             margin: 0 auto;
         }
+
+        .component1-container {
+            margin-bottom: 240px;
+        }
+
         .heading3 {
             font-size: 26px !important;
             font-weight: 200 !important;
@@ -38,8 +44,9 @@ const AppStyles = createTheme({
             width: 100%;
         }
 
-        .heading3, .heading6 {
+        .heading2, .heading3, .heading6 {
             text-transform: uppercase;
+            font-weight: 300;
         }
 
         .heading6 {
@@ -47,11 +54,18 @@ const AppStyles = createTheme({
             font-size: 16px !important;
         }
 
+        .heading2 {
+            text-align: center;
+            font-size: 43px;
+            color: #fff;
+        }
+
         .component1-element1 {
             padding-bottom: 35px;
             line-height: 1.5;
             font-size: 18px;
             letter-spacing: 1px;
+            color: #dfe0e0 !important;
         }
 
         .component2-element2 {
@@ -59,6 +73,54 @@ const AppStyles = createTheme({
             line-height: 1.5; 
             letter-spacing: 1px;
             font-size: 18px;
+        }
+
+        .card-component {
+            border-radius: 0px !important;
+            background: transparent !important;
+            color: #fff !important;
+            box-shadow: none !important;
+        }
+
+        .card-component-content-media {
+            border-bottom: 5px solid #cf1430;
+        }
+
+        .card-component-content{
+            padding: 20px 0px 0px 0px !important;
+            color: #b2b3b3 !important;
+        }
+        
+        .card-component-content-title{
+            font-size: 18px !important;
+            color: #fff !important;
+            margin-bottom: 12px !important;
+        }
+
+        .card-component-content-description{
+            color: #b2b3b3 !important;
+            font-size: 16px !important;
+            line-height: 1.75 !important;
+        }
+
+        .card-component-action {
+            padding: 8px 0px !important;
+        }
+
+        .card-component-action > div {
+            cursor: pointer !important;
+            color: #fff;
+            font-weight: 400;
+            -webkit-user-select: none; /* Safari */
+            -ms-user-select: none; /* IE 10 and IE 11 */
+            user-select: none; /* Standard syntax */
+          
+        }
+
+        .card-component-action > div > span {
+            padding-bottom: 10px;
+            border-bottom: 1px solid #cf1430;
+            line-height: 48px;
         }
         `,
     },
---
+++ src/screen/Home/component/component2.tsx
@@ -0,0 +1,53 @@
+import { Box, Grid } from '@mui/material';
+import Image1 from '../../../assets/component-02/Image-01.jpg';
+import Image2 from '../../../assets/component-02/Image-02.jpg';
+import Image3 from '../../../assets/component-02/Image-03.jpg';
+import CardComponent from './CardComponent/CardComponent';
+
+const Component1 = () => {
+  const readMore = {
+    label: 'READ MORE',
+    onClick: () => console.log('Read more'),
+  };
+  return (
+    <Box className="component2-container">
+      <Box component="h2" className="heading2">
+        All the latest from AEG
+      </Box>
+      <Grid container spacing={2}>
+        <Grid item md={12} xl={4}>
+          <CardComponent
+            imageAlt="image-1"
+            image={Image1}
+            title="Summer Lunch Menu By Mark Best"
+            description="AEG ambassador Mark&#39;s Bests summer eats are guaranteed to help you make the most of the warmer weather and entertaining at home."
+            readMore={readMore}
+          />
+        </Grid>
+        <Grid item md={12} xl={4}>
+          <CardComponent
+            imageAlt="image-2"
+            image={Image2}
+            title="A Traditional Christmas Eve, Mark Best Style"
+            description="One of Australia's best chefs and AEG ambassador, Mark Best, shares his favourite Christmas Eve menu which is sure to impress your guests.
+            "
+            readMore={readMore}
+          />
+        </Grid>
+        <Grid item md={12} xl={4}>
+          <CardComponent
+            imageAlt="image-3"
+            image={Image3}
+            title="Taking Taste Further"
+            description="This exclusive cookbook gives you all the know-how you need. We&#39;ve designed it to make sure you get the most out of our products - and the best out of your
+            food.
+            "
+            readMore={readMore}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Component1;

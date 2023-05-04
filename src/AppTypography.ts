e679a5d70dd21a01d625a5f9728900528e25404a
alec.victor <victoralec08@gmail.com>
2023-05-04 18:21:22 +0800

    Create initial layout for component 1

---
+++ src/AppTypography.ts
@@ -0,0 +1,20 @@
+import { TypographyOptions } from '@mui/material/styles/createTypography';
+
+import '@fontsource/open-sans';
+
+const appTypography: TypographyOptions = {
+  fontFamily: '"Open Sans", "Arial", "sans-serif"',
+  fontWeightLight: 200,
+  fontWeightRegular: 400,
+  fontWeightMedium: 500,
+  fontWeightBold: 600,
+
+  // h3: {
+  //   fontFamily: '"Open Sans"',
+  //   fontSize: '3rem',
+  //   fontWeight: 200,
+  //   textTransform: 'none',
+  // },
+};
+
+export default appTypography;
